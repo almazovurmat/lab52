7c0a1d297e83b596b208add4b6615cc386cc0ffc
Almazov Urmat <almazov.urmat@gmail.com>
2023-05-19 14:13:21 +0600

    Added component "CardView" to project

---
+++ src/CardView/CardView.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface ICardViewProps {
+    rank: "";
+    suit: string;
+}
+
+
+const CardView: React.FC <ICardViewProps> = props => {
+
+    const cardClasses = `props.rank-${props.rank.toLowerCase()} ${props.suit}`;
+
+    let suit;
+    switch (props.suit) {
+        case 'diams':
+            suit = '♦';
+            break;
+        case 'hearts':
+            suit = '♥';
+            break;
+        case 'clubs':
+            suit = '♣';
+            break;
+        case 'spades':
+            suit = '♠';
+            break;
+        default:
+            suit = 'Some Error';
+            break;
+    }
+
+    return (
+        <div className="playingCards faceImages">
+            <span className={cardClasses}>
+                <span className="rank">{props.rank}</span>
+                <span className="suit">{suit}</span>
+            </span>
+        </div>
+    );
+};
+
+export default CardView;
