0370a72ce8457bef689e92389b58e3ebc3614d4e
Almazov Urmat <almazov.urmat@gmail.com>
2023-05-19 16:15:30 +0600

    draw cards on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CardView from "./CardView/CardView";
+import CardDeck from "./lib/CardDeck";
 import './App.css';
+import Card from "./lib/Card";
 
 const App = () => {
+    const [cards, setCards] = useState<Card[]> ([]);
+
+    const distributeCards = () => {
+        setCards((new CardDeck().getCards(5)));
+    };
+
+    if (cards.length <= 0) {
+        return <button onClick={distributeCards}>Раздать карты</button>
+    }
+
     return (
         <div className="App">
-            <CardView rank="" suit="" />
+            {
+                cards.map(card => {
+                    return <CardView key={card.rank+card.suit} rank={card.rank} suit={card.suit}/>
+                })
+            }
+            <button onClick={distributeCards}>Раздать карты</button>
         </div>
     );
 };
